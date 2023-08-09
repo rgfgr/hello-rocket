dc19d09f5640cdd7592600e2004ffd8c0237e612
rgfgr <88767865+rgfgr@users.noreply.github.com>
2023-08-09 09:53:39 +0200

    Forwarding

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,22 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/user/<id>")]
+fn user(id: usize) -> String {
+    format!("your id: {}, is a unsigned integer", id)
+}
+
+#[get("/user/<id>", rank = 2)]
+fn user_int(id: isize) -> String {
+    format!("your id: {}, is a signed integer", id)
+}
+
+#[get("/user/<id>", rank = 3)]
+fn user_str(id: &str) -> String {
+    format!("your id: {}, is a string", id)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, user, user_int, user_str])
 }
